2055ee47a632bed1d299299c5e883d24c026aca7
tszyrowski <tszyrowski@gmail.com>
2019-08-24 20:37:25 +0100

    basic figure object

---
+++ src/playing_with_bokeh/basic_vis.py
@@ -0,0 +1,22 @@
+"""
+ Created on 24 Aug 2019
+"""
+import pandas as pd
+import numpy as np
+
+from bokeh.io import output_file, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
+from bokeh.models.widgets import Tabs, Panel
+from src.playing_with_bokeh import plots_output
+
+# The data will be created as name.html
+output_file(plots_output.plot_out('firs_plot'))
+# render inline with jupiter notebook
+# output_notebook() # will give an error if no Ipython installed
+
+
+fig = figure() # inistantiate a figure objet
+
+show(fig)
---
+++ src/playing_with_bokeh/plots_output.py
@@ -0,0 +1,21 @@
+"""
+Return path to file in output file folder
+"""
+import pathlib
+
+
+def plot_out(plot_name):
+    """Os indenpendent path to a folder where plots are stored
+    
+    params:
+        plot_name - name of the output plot taken to {name}.html 
+    """
+    name = "{}.html".format(plot_name)
+    plot_path = pathlib.Path(__file__).resolve().parent.parent.parent
+    plot_path = pathlib.Path.joinpath(plot_path, "plots")
+    if not plot_path.exists():
+        plot_path.mkdir(parents=True, exist_ok=True)
+    return pathlib.Path.joinpath(plot_path, name)
+
+if __name__=="__main__":
+    print(plot_out("plot_name"))
